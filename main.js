0649701f5ad5f520e980f09f30556add6889321e
tomas-nogueira <129890262+tomas-nogueira@users.noreply.github.com>
2023-04-05 11:21:39 -0300

    Alterações js

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-document.addEventListener("click" , showcolor );
+document.addEventListener( "click" , showcolor );
+
+document.getElementById("botao").addEventListener( "click", showname );
 
 function showcolor ()
 {
     document.body.style.backgroundColor = "red";
+}
+
+function showname()
+{
+    let usario = document.getElementById ("nome").value;
+    document.getElementById("mensagem").innerText = "Meu nome é" + usuario;
 }
